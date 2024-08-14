b53cd26c01986b4782c452fb12982ddc42f5814a
John Okall <Jhnprimee@gmail.com>
2024-08-14 12:26:37 +0300

    Added Assets and Custom CSS variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin', 'latin-ext'],
+  weight: ['300', '400', '500', '600'],
+  variable: '--ff-poppins'});
 
 export const metadata: Metadata = {
   title: "Okall | Portfolio",
@@ -16,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
